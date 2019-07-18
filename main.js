cba7046fb7994548409d41fec2ada8c57808f413
unknown <bocalist@gmail.com>
2019-07-18 18:00:06 +0900

    communication between component and root

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+Vue.component('modal', {
+
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">This is modal</div>
+            </div>
+            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+        </div>
+    `,
+
+});
+
+
+
 Vue.component('message', {
     data() {
         return {
@@ -17,7 +33,7 @@ Vue.component('message', {
         </article>
     `,
     
-})
+});
 
 
 Vue.component('task', {
@@ -50,13 +66,14 @@ new Vue({
     data: {
         title: 'Add this name',
         newName: '',
-        names: [ 'John', 'Sansa', 'Aria', 'Rob', 'Ned' ]
+        names: [ 'John', 'Sansa', 'Aria', 'Rob', 'Ned' ],
+        showModal: false,
     },
 
     methods: {
         addName() {
             this.names.push(this.newName);
             this.newName = '';
-        }
+        }, 
     }
 });
