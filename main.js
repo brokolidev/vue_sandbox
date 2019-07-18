52fc4fdc8fb160893febe07b2807317deacf1c6e
unknown <bocalist@gmail.com>
2019-07-18 18:54:57 +0900

    bind tab functions

--- main.js
+++ main.js
@@ -1,3 +1,70 @@
+Vue.component('tabs', {
+    data() {
+        return {
+            tabs: [],
+        }
+    },
+
+    template: `
+    <div class="box">
+        <div class="tabs">
+            <ul>
+                <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                    <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="tabs-details">
+            <slot></slot>
+        </div>
+    </div>
+    `,
+
+    created() {
+        this.tabs = this.$children;
+    },
+
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            });
+        }
+    }
+
+})
+
+Vue.component('tab', {
+    data() {
+        return {
+            isActive: false
+        }
+    },
+
+    props: {
+        name: { required: true },
+        selected: { default: false },
+    },
+
+    template: `
+    <div class="contents" v-show="isActive">
+        <p><slot></slot></p>
+    </div>
+    `,
+
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+
+    mounted() {
+        this.isActive = this.selected;
+    }
+})
+
+
 Vue.component('modal', {
 
     template: `
